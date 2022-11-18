c07b9e889f0c8428ca540818d67b8f4575b2db6f
Loosen-IT <janniclas@loosen-it.de>
2022-11-18 11:21:29 +0100

    Index Page Part 1
    Aufgabe 01

---
+++ tmp.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Weiterleitung</title>
+</head>
+<body>
+    <p>HREF works fine!</p>
+    <a href="index.php">Back to Index!</a>
+
+</body>
+</html>
