67eae47609de4de61e3763d936592ccd25434e8f
C0deEater <117350097+C0deEater@users.noreply.github.com>
2023-01-05 22:45:03 +0100

    Model
    Model hinzugefügt

--- codeigniter/app/Controllers/Mitglieder.php
+++ codeigniter/app/Controllers/Mitglieder.php
@@ -2,9 +2,15 @@
 
 namespace App\Controllers;
 use CodeIgniter\Controller;
+use App\Models\MitgliederModel;
 
 class Mitglieder extends SessionController
 {
+
+    public function __construct(){
+        $this->MitgliederModel = new MitgliederModel();
+    }
+
     public function index()
     {
         $this->session_parameters();
@@ -12,7 +18,7 @@ class Mitglieder extends SessionController
         $data['INFO_title'] = "Aufgabenplaner: Mitglieder";
         $data['CSS_bootstrap'] = base_url().'/codeigniter/public/styles/bootstrap.css';
         $data['CSS_custom'] = base_url().'/codeigniter/public/styles/custom.css';
-        $data['DATA_mitglieder'] = $this->create_mitglieder();
+        $data['DATA_mitglieder'] = $this->MitgliederModel->getMitglieder();
         $data['DATA_projekte_mitglieder'] = $this->create_projekte_mitglieder();
 
         echo view('templates/head.php', $data);
---
+++ codeigniter/app/Models/MitgliederModel.php
@@ -0,0 +1,57 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MitgliederModel extends Model {
+
+
+    public function getMitglieder ($mitglieder_id = NULL) {
+        $this->mitglieder = $this->db->table('mitglieder');
+        $this->mitglieder->select('*');
+
+        IF ($mitglieder_id != NULL)
+            $this->mitglieder->where('mitglieder.id_mitglieder', $mitglieder_id);
+
+        $this->mitglieder->orderBy('username');
+        $result = $this->mitglieder->get();
+
+        if ($mitglieder_id != NULL)
+            return $result->getRowArray();
+        else
+            return $result->getResultArray();
+    }
+
+    public function createMitglied() {
+
+        $this->mitglieder = $this->db->table('mitglieder');
+        $this->mitglieder->insert(array(
+            'username' => $_POST['username'],
+            'passwort' => $_POST['passwort'],
+            'email' => $_POST['email']));
+    }
+
+    public function updateMitglied() {
+
+        $this->mitglieder = $this->db->table('mitglieder');
+        $this->mitglieder->where('mitglieder.id_mitglieder', $_POST['id_mitglieder']);
+        $this->mitglieder->update(array(
+            'username' => $_POST['username'],
+            'passwort' => $_POST['passwort'],
+            'email' => $_POST['email']));
+    }
+
+    public function deleteMitglied() {
+        $this->mitglieder = $this->db->table('mitglieder');
+        $this->mitglieder->where('mitglieder.id_mitglieder', $_POST['id_mitglieder']);
+        $this->mitglieder->delete();
+    }
+
+    public function loginMitglied() {
+        $this->mitglieder = $this->db->table('mitglieder');
+        $this->mitglieder->select('passwort');
+        $this->mitglieder->where('mitglieder.username', $_POST['username']);
+        $ausgabe = $this->mitglieder->get();
+
+        return $ausgabe->getRowArray();
+    }
+}
