4dee8c68ed5773ce261eab413f817fb267942b69
Jan-Niclas Loosen <janniclas@loosen-it.de>
2023-01-30 12:27:13 +0100

    Update head.php

--- app/Views/templates/head.php
+++ app/Views/templates/head.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="<?= $CSS_custom; ?>">
     <title><?= $INFO_title; ?></title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <link rel="icon" href="<?php echo base_url(); ?>/favicon.svg" type="svg">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" > </script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" ></script>
 </head>
